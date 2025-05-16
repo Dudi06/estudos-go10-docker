c226454b2d268726bd81cefe11f8d84607f5a190
Dudi06 <neoxys1@gmail.com>
2025-05-16 13:42:49 -0300

    Progressão atual

--- graphql/account_resolver.go
+++ graphql/account_resolver.go
@@ -1,1 +1,9 @@
-Orders
+package main
+
+type accountResolver struct {
+	server *Server
+}
+
+/*func (r *queryResolver) Orders(ctx context.Context, obj *Account) (*Order, error) {
+
+}*/
--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,5 +1,17 @@
-CreateAccount
+package main
 
-CreateProduct
+type mutationResolver struct {
+	server *Server
+}
 
-CreateOrder
+/*func (r *mutationResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
+
+}
+
+func (r *mutationResolver) createProduct(ctx context.Context, in ProductInput) (*Product, error) {
+
+}
+
+func (r *mutationResolver) createOrder(ctx context.Context, in OrderInput) (*Order, error) {
+
+}*/
